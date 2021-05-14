0d95c0136925e67b133ce4efd1f4c8c61f3d9ef9
multidissimo <84193720+multidissimo@users.noreply.github.com>
2021-05-14 17:24:14 +0200

    Create general.js

---
+++ general.js
@@ -0,0 +1,28 @@
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+var version = '[Alpha] ';
+
+module.exports = async (event, context) => {
+  
+  /**  if(event.content.match(/test/i)) {
+    await lib.discord.channels.messages.create({
+      channel_id: event.channel_id,
+      content: 'test',
+    })
+  }*/
+  
+  if ((event.content.match(/(basket of seeds)|(nether wart pouch)|(builders wand)|(builder's wand)|(builder wand)/i)) 
+  && (event.content.match(/aktiviert|deaktiviert|(geht nicht)|wann|warum/i))) {
+    await lib.discord.channels.messages.create({
+      channel_id: event.channel_id,
+      content: ``,
+      embed: {
+        title: version + 'Automatische Antwort',
+        type: 'rich',
+        color: 0xFF8C00,
+        description: 'Basket of Seeds, Nether Wart Pouch, Builder`s Wand etc. sind derzeit deaktiviert. Es ist nicht bekannt, wann sie wieder funktionieren werden.',
+      }
+    });
+  }
+  
+}
